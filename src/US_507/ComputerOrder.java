299d997a680fa04a625a690e95177eb479144b75
dilanoguzhan <dilan.selimoglu@gmail.com>
2024-04-04 23:25:19 +0200

    TC507 ComputerOrder added.

--- src/US_507/ComputerOrder.java
+++ src/US_507/ComputerOrder.java
@@ -1,4 +1,50 @@
 package US_507;
 
-public class ComputerOrder {
+import Utility.BaseDriver;
+import Utility.Tools;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
+
+public class ComputerOrder extends BaseDriver {
+
+
+    @Test(groups = {"UI Test", "TABMenu Test", "Order Test"})
+    public void TC_507() {
+
+        ComputerOrder_Elements coe = new ComputerOrder_Elements();
+
+        Assert.assertTrue(coe.tabMenu.get(0).getText().contains("Computers"), "Tab menu did not display");
+        Assert.assertTrue(coe.tabMenu.get(1).getText().trim().equals("Electronics"), "Tab menu did not display");
+        Assert.assertEquals(coe.tabMenu.get(2).getText().trim(), "Apparel", "Tab menu did not display");
+        Tools.ActionHover(coe.computers);
+        Assert.assertTrue(coe.computersSubList.get(0).getText().trim().equals("Desktops"), "Desktops did not display");
+        coe.computersSubList.get(0).click();
+        wait.until(ExpectedConditions.urlContains("desktops"));
+        Assert.assertTrue(coe.productList.get(0).getText().contains("Build"), "Desktop did not display");
+        coe.productList.get(0).click();
+        wait.until(ExpectedConditions.urlContains("build-your-own-computer"));
+
+        int random = Tools.randomGenerator(coe.ramChoice.size());
+        coe.ramChoice.get(random).click();
+
+        int random1 = Tools.randomGenerator(coe.harddiscChoice.size());
+        Tools.JavaScriptClick(coe.harddiscChoice.get(random1));
+
+        wait.until(ExpectedConditions.elementToBeClickable(coe.addCart));
+        Tools.JavaScriptClick(coe.addCart);
+
+        coe.greenAddCart.click();
+
+        wait.until(ExpectedConditions.visibilityOf(coe.priceValue2));
+       // Assert.assertEquals(coe.priceValue1.getText(), coe.priceValue2.getText(), "The price is not same.");
+        //String price1="";
+
+
+    }
 }
--- src/US_507/ComputerOrder_Elements.java
+++ src/US_507/ComputerOrder_Elements.java
@@ -1,4 +1,85 @@
 package US_507;
 
+import org.openqa.selenium.WebElement;
+import Utility.BaseDriver;
+import org.openqa.selenium.support.*;
+
+import java.util.List;
+
 public class ComputerOrder_Elements {
+    public ComputerOrder_Elements() {
+        PageFactory.initElements(BaseDriver.driver, this);
+    }
+
+    @FindBy(css = "[class^='sender-name']")
+    public WebElement yourName;
+
+    @FindBy(css = ".sender-email")
+    public WebElement yourEmail;
+
+    @FindBy(xpath = "//div[@class='header-links']/ul/li[1]/a")
+    public WebElement myAccount;
+
+    @FindBy(id = "small-searchterms")
+    public WebElement searchBox;
+
+    @FindBy(xpath = "//button[text()='Search']")
+    public WebElement searchButton;
+
+    @FindBy(xpath = "//div[@class='product-item']/div[2]/h2/a")
+    public WebElement productTitle;
+
+    @FindBy(xpath = "//ul[@class='top-menu notmobile']/li/a")
+    public List<WebElement> tabMenu;
+
+    // public WebElement ortakTab;
+
+    @FindBy(css = "[class='top-menu notmobile'] [href='/computers']")
+    //(//ul[@class='top-menu notmobile']/li/a)[1]  diger yöntem xpath
+    public WebElement computers;
+
+    @FindBy(css = "[class='title'] [href='/desktops']")
+    public WebElement desktops;
+
+    @FindBy(css = "[class='product-item'][data-productid='1']")
+    public WebElement buildOwnComputer;
+
+    //  @FindBy(id = "product_attribute_2")
+    // public WebElement ramChoice;
+
+    @FindBy(xpath = "//select[@id='product_attribute_2']/option[@data-attr-value]")
+    public List<WebElement> ramChoice;
+
+    @FindBy(css = "[id*='product_attribute_3_']")
+    public List<WebElement> harddiscChoice;
+
+    @FindBy(css = "[class='bar-notification success']")
+    public WebElement addToCartWarning;
+
+    @FindBy(css = "[class='product-viewmode'] a:nth-child(3)")
+    public WebElement viewMode;
+
+    @FindBy(xpath = "(//ul[@class='sublist first-level'])[1]//a")
+    public List<WebElement> computersSubList;
+
+
+    @FindBy(css = "[class='product-title'] a")
+    public List<WebElement> productList;
+
+    @FindBy(id = "add-to-cart-button-1")
+    public WebElement addCart;
+
+    @FindBy(xpath = "//li[@id='topcartlink']/a")
+    public WebElement gotoCart;
+
+    @FindBy(xpath = "//p[@class='content']/a")
+    public WebElement greenAddCart;
+
+    @FindBy(xpath ="//span[@id='price-value-1']" )
+    public WebElement priceValue1;
+//id = "price-value-1"
+    @FindBy(xpath = "//span[@class='product-unit-price']")
+    public WebElement priceValue2;
+
+
 }
